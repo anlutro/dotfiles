9f894513bcf6cb5418a333af9b2590f23a2145ae
Andreas Lutro <anlutro@gmail.com>
2014-01-06 21:45:58 +0100

    fix script

--- symlink.sh
+++ symlink.sh
@@ -5,12 +5,13 @@ BASEDIR=$(dirname $0)
 for file in $BASEDIR/.*; do
 	if [ -f $file ]; then
 		FN=$(basename $file)
+		src=$(readlink -m $file)
 		echo Linking $FN ...
 
 		if [ -f ~/$FN ]; then
 			rm ~/$FN
 		fi
 
-		ln -s $file ~/$FN
+		ln -s $src ~/$FN
 	fi
 done
