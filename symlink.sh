38d77c089a8df4616b8d8fd0aebae373854dc95f
Andreas Lutro <anlutro@gmail.com>
2014-01-21 19:43:13 +0100

    add bin

--- symlink.sh
+++ symlink.sh
@@ -4,14 +4,30 @@ BASEDIR=$(dirname $0)
 
 for file in $BASEDIR/.*; do
 	if [ -f $file ]; then
-		FN=$(basename $file)
-		src=$(readlink -m $file)
-		echo Linking $FN ...
+		filename=$(basename $file)
+		srcpath=$(readlink -m $file)
+		trgpath=~/$filename
+		echo Linking $filename ...
 
-		if [ -f ~/$FN ]; then
-			rm ~/$FN
+		if [ -f $trgpath ]; then
+			rm $trgpath
 		fi
 
-		ln -s $src ~/$FN
+		ln -s $srcpath $trgpath
+	fi
+done
+
+for file in $BASEDIR/bin/*; do
+	if [ -f $file ]; then
+		filename=$(basename $file)
+		srcpath=$(readlink -m $file)
+		trgpath=~/bin/$filename
+		echo Linking $filename ...
+
+		if [ -f $trgpath ]; then
+			rm $trgpath
+		fi
+
+		ln -s $srcpath $trgpath
 	fi
 done
