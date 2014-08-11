8f57cf3d312861d3df1b1d525b8e737665146e7d
Andreas Lutro <anlutro@gmail.com>
2014-08-11 18:39:23 +0200

    change ps1 color in ssh

--- bash/rc.sh
+++ bash/rc.sh
@@ -43,7 +43,12 @@ if [ -n "$force_color_prompt" ]; then
 fi
 
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)")\n\$ '
+    if [ -n "$SSH_CLIENT" ]; then
+        color="31" # red
+    else
+        color="32" # green
+    fi
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;'$color'm\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(__git_ps1 " (%s)")\n\$ '
 else
     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\n\$ '
 fi
