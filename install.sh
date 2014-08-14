abb45f0c9f3bf5f808dd0c4ad6592e7c7724e4a0
Andreas Lutro <anlutro@gmail.com>
2014-08-14 09:03:34 +0200

    fix install conditionals

--- install.sh
+++ install.sh
@@ -3,26 +3,32 @@
 # script directory
 sd=$(dirname $(readlink -f "$0"))
 
+echo "Linking bash files..."
 ln -sf $sd/bash/aliases.sh ~/.bash_aliases
 ln -sf $sd/bash/rc.sh ~/.bashrc
 
-if [[ hash git 2>/dev/null ]]; then
+if command -v git >/dev/null 2>&1; then
+	echo "Linking git files..."
 	ln -sf $sd/git/config ~/.gitconfig
 	ln -sf $sd/git/ignore_global ~/.gitignore_global
 fi
 
-if [[ hash conky 2>/dev/null ]]; then
+if command -v conky >/dev/null 2>&1; then
+	echo "Linking conkyrc..."
 	ln -sf $sd/conkyrc ~/.conkyrc
 fi
 
-if [[ hash tmux 2>/dev/null ]]; then
+if command -v tmux >/dev/null 2>&1; then
+	echo "Linking tmux.conf..."
 	ln -sf $sd/tmux.conf ~/.tmux.conf
 fi
 
-if [[ hash colordiff 2>/dev/null ]]; then
+if command -v colordiff >/dev/null 2>&1; then
+	echo "Linking colordiffrc..."
 	ln -sf $sd/colordiffrc ~/.colordiffrc
 fi
 
-if [[ hash openbox 2>/dev/null ]]; then
+if command -v openbox >/dev/null 2>&1; then
+	echo "Linking openbox config dir..."
 	ln -sfT $sd/openbox ~/.config/openbox
 fi
