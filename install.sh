d1ce8742f93977a8ba958aec5296526d7aaf9718
Andreas Lutro <anlutro@gmail.com>
2014-11-20 20:37:37 +0100

    add bin files. simplify menu

--- install.sh
+++ install.sh
@@ -3,52 +3,60 @@
 # script directory
 sd=$(dirname $(readlink -f "$0"))
 
-echo "Linking bash files..."
+echo "Linking bash files ..."
 ln -sf $sd/bash/aliases.sh ~/.bash_aliases
 ln -sf $sd/bash/rc.sh ~/.bashrc
 
 if command -v nano >/dev/null 2>&1; then
-	echo "Linking nanorc..."
+	echo "Linking nanorc ..."
 	ln -sf $sd/nanorc ~/.nanorc
 fi
 
 if command -v git >/dev/null 2>&1; then
-	echo "Linking git files..."
+	echo "Linking git files ..."
 	ln -sf $sd/git/config ~/.gitconfig
 	ln -sf $sd/git/ignore_global ~/.gitignore_global
 fi
 
 if command -v conky >/dev/null 2>&1; then
-	echo "Linking conkyrc..."
+	echo "Linking conkyrc ..."
 	ln -sf $sd/conkyrc ~/.conkyrc
 fi
 
 if command -v tmux >/dev/null 2>&1; then
-	echo "Linking tmux.conf..."
+	echo "Linking tmux.conf ..."
 	ln -sf $sd/tmux.conf ~/.tmux.conf
 fi
 
 if command -v colordiff >/dev/null 2>&1; then
-	echo "Linking colordiffrc..."
+	echo "Linking colordiffrc ..."
 	ln -sf $sd/colordiffrc ~/.colordiffrc
 fi
 
 if command -v openbox >/dev/null 2>&1; then
-	echo "Linking openbox config dir..."
+	echo "Linking openbox config dir ..."
 	ln -sfT $sd/openbox ~/.config/openbox
 fi
 
 if command -v tint2 >/dev/null 2>&1; then
-	echo "Linking tint2 config dir..."
+	echo "Linking tint2 config dir ..."
 	ln -sfT $sd/tint2 ~/.config/tint2
 fi
 
-if command -v x-terminal-emulator >/dev/null 2>&1; then
-	echo "Linking x-terminal-emulator config dir..."
+if command -v xfce4-terminal >/dev/null 2>&1; then
+	echo "Linking xfce4-terminal config dir ..."
 	ln -sfT $sd/xfce4/terminal ~/.config/xfce4/terminal
 fi
 
 if command -v terminator >/dev/null 2>&1; then
-	echo "Linking terminator config dir..."
+	echo "Linking terminator config dir ..."
 	ln -sfT $sd/terminator ~/.config/terminator
 fi
+
+# Link all files in ./bin to ~/bin
+for f in $sd/bin/*; do
+	file=$(basename $f)
+	path=$(readlink -f $f)
+	ln -sf $path ~/bin/$file
+	echo "Linking bin/$file ..."
+done
