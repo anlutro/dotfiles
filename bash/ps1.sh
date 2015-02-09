d07a481dc9099e42f20f60bc0d6ccada04711f7e
Andreas Lutro <anlutro@gmail.com>
2015-02-09 18:02:25 +0100

    tweak ps1

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -1,18 +1,28 @@
-if [ $(whoami) = "vagrant" ]; then
-    color="33" # yellow
+whoami=$(whoami)
+
+if [ $whoami = 'vagrant' ]; then
+    color='34' # blue
 elif [ -n "$SSH_CLIENT" ]; then
-    color="31" # red
+	if [ $whoami = 'root' ]; then
+	    color='31' # red
+	else
+	    color='34' # yellow
+	fi
 else
-    color="32" # green
+    color='32' # green
 fi
 
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;'$color'm\]\u@\h\[\033[00m\]:\[\033[00;37m\]\w\[\033[00m\]'
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;'$color'm\]\u@\h\[\033[00m\] \w'
 
 if command -v __git_ps1 >/dev/null 2>&1; then
+	# GIT_PS1_SHOWDIRTYSTATE=true
+	# GIT_PS1_SHOWUNTRACKEDFILES=true
+	# GIT_PS1_SHOWUPSTREAM="auto"
+	# GIT_PS1_SHOWCOLORHINTS=true
     PS1=$PS1'$(__git_ps1 " (%s)")'
 fi
 
-if [ $(whoami) = 'root' ]; then
+if [ $whoami = 'root' ]; then
     PS1=$PS1'\n# '
 else
     PS1=$PS1'\n$ '
