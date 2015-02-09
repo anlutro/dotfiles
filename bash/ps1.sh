c6916190068d7ce639ab8f0e264044b170ac3a7f
Andreas Lutro <anlutro@gmail.com>
2015-02-09 13:30:58 +0100

    prettier colors

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -6,7 +6,7 @@ else
     color="32" # green
 fi
 
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;'$color'm\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]'
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;'$color'm\]\u@\h\[\033[00m\]:\[\033[00;37m\]\w\[\033[00m\]'
 
 if command -v __git_ps1 >/dev/null 2>&1; then
     PS1=$PS1'$(__git_ps1 " (%s)")'
