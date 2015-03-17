93bac3fb54dbf859bed1fc59ca439adeb13f0dac
Andreas Lutro <anlutro@gmail.com>
2015-03-17 19:27:11 +0100

    add i3lock alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -57,6 +57,7 @@ function py3help {
 # misc
 alias crlffix='for file in `find . -type f`; do dos2unix $file; done'
 alias ss='sudo service'
+alias i3lock='i3lock -c 000000'
 
 # livestreamer
 function s {
