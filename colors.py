847ec35ec739759a7aa60c5c262a5544df6a1be9
Andreas Lutro <anlutro@gmail.com>
2015-08-31 18:13:17 +0200

    improve color script

--- colors.py
+++ colors.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
-write = sys.stdout.write
+def write_color(color):
+	sys.stdout.write("\33[%sm %s \33[m " % (color, color))
 for i in range(2):
-    for j in range(30, 38):
-        for k in range(40, 48):
-            write("\33[%d;%d;%dm %d;%d;%d \33[m " % (i, j, k, i, j, k))
-        write("\n")
+	for j in range(30, 38):
+		write_color('%d;%d' % (i, j))
+		for k in range(40, 48):
+			write_color('%d;%d;%d' % (i, j, k))
+		sys.stdout.write("\n")
