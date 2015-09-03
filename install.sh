d876089fa3da203111352213a13b026e34aa399e
Andreas Lutro <anlutro@gmail.com>
2015-09-03 08:59:29 +0200

    move gtk-theme management to vendor

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$HOME" ]; then
+	echo '$HOME is empty, aborting'
+	exit 1
+fi
+
 # script directory
 sd=$(dirname $(readlink -f "$0"))
 # vendor directory
@@ -13,76 +18,76 @@ install() {
 }
 
 install_bash() {
-	ln -sf $sd/bash/bashrc ~/.bashrc
-	ln -sf $sd/bash/aliases ~/.bash_aliases
-	ln -sf $sd/bash/ps1 ~/.bash_ps1
+	ln -sf $sd/bash/bashrc $HOME/.bashrc
+	ln -sf $sd/bash/aliases $HOME/.bash_aliases
+	ln -sf $sd/bash/ps1 $HOME/.bash_ps1
 }
 
 install_compton() {
-	ln -sf $sd/compton.conf ~/.config/compton.conf
+	ln -sf $sd/compton.conf $HOME/.config/compton.conf
 }
 
 install_conky() {
-	ln -sf $sd/conkyrc ~/.conkyrc
+	ln -sf $sd/conkyrc $HOME/.conkyrc
 }
 
 install_dunst() {
-	ln -sfT $sd/dunst ~/.config/dunst
+	ln -sfT $sd/dunst $HOME/.config/dunst
 }
 
 install_git() {
-	ln -sf $sd/git/config ~/.gitconfig
-	ln -sf $sd/git/ignore_global ~/.gitignore_global
+	ln -sf $sd/git/config $HOME/.gitconfig
+	ln -sf $sd/git/ignore_global $HOME/.gitignore_global
 }
 
 install_gsimplecal() {
-	ln -sfT $sd/gsimplecal ~/.config/gsimplecal
+	ln -sfT $sd/gsimplecal $HOME/.config/gsimplecal
 }
 
 install_i3() {
-	ln -sfT $sd/i3 ~/.config/i3
+	ln -sfT $sd/i3 $HOME/.config/i3
 }
 
 install_i3status() {
-	ln -sfT $sd/i3status ~/.config/i3status
+	ln -sfT $sd/i3status $HOME/.config/i3status
 }
 
 install_irssi() {
-	[ -d ~/.irssi ] || mkdir -p ~/.irssi
-	ln -sf $sd/irssi/default.theme ~/.irssi/default.theme
+	[ -d $HOME/.irssi ] || mkdir -p $HOME/.irssi
+	ln -sf $sd/irssi/default.theme $HOME/.irssi/default.theme
 }
 
 install_mutt() {
-	ln -sfT $sd/muttrc ~/.muttrc
+	ln -sfT $sd/muttrc $HOME/.muttrc
 }
 
 install_nano() {
-	ln -sf $sd/nanorc ~/.nanorc
+	ln -sf $sd/nanorc $HOME/.nanorc
 }
 
 install_openbox() {
-	ln -sfT $sd/openbox ~/.config/openbox
+	ln -sfT $sd/openbox $HOME/.config/openbox
 }
 
 install_rtorrent() {
-	ln -sf $sd/rtorrent.rc ~/.rtorrent.rc
+	ln -sf $sd/rtorrent.rc $HOME/.rtorrent.rc
 }
 
 install_subl() {
-	ln -sfT $sd/sublime-text ~/.config/sublime-text-3/Packages/User
-	ln -sf $sd/bin/sublp ~/bin/sublp
+	ln -sfT $sd/sublime-text $HOME/.config/sublime-text-3/Packages/User
+	ln -sf $sd/bin/sublp $HOME/bin/sublp
 }
 
 install_terminator() {
-	ln -sfT $sd/terminator ~/.config/terminator
+	ln -sfT $sd/terminator $HOME/.config/terminator
 }
 
 install_tint2() {
-	ln -sf $sd/tint2/tint2rc ~/.config/tint2/tint2rc
+	ln -sf $sd/tint2/tint2rc $HOME/.config/tint2/tint2rc
 }
 
 install_tmux() {
-	ln -sf $sd/tmux.conf ~/.tmux.conf
+	ln -sf $sd/tmux.conf $HOME/.tmux.conf
 }
 
 install_urxvt() {
@@ -90,10 +95,10 @@ install_urxvt() {
 		git clone https://github.com/muennich/urxvt-perls $vd/urxvt-perls
 	fi
 
-	[ -d ~/.urxvt/ext ] || mkdir -p ~/.urxvt/ext
-	ln -sf $vd/urxvt-perls/url-select ~/.urxvt/ext/url-select
-	ln -sf $vd/urxvt-perls/clipboard ~/.urxvt/ext/clipboard
-	ln -sf $vd/urxvt-perls/keyboard-select ~/.urxvt/ext/keyboard-select
+	[ -d $HOME/.urxvt/ext ] || mkdir -p $HOME/.urxvt/ext
+	ln -sf "$vd/urxvt-perls/url-select" $HOME/.urxvt/ext/url-select
+	ln -sf "$vd/urxvt-perls/clipboard" $HOME/.urxvt/ext/clipboard
+	ln -sf "$vd/urxvt-perls/keyboard-select" $HOME/.urxvt/ext/keyboard-select
 }
 
 vim_common() {
@@ -105,36 +110,36 @@ vim_common() {
 }
 install_vim() {
 	vim_common
-	ln -sfT $sd/vimrc ~/.vimrc
-	ln -sfT $sd/vim ~/.vim
+	ln -sfT $sd/vimrc $HOME/.vimrc
+	ln -sfT $sd/vim $HOME/.vim
 }
 install_nvim() {
 	vim_common
-	ln -sfT $sd/vimrc ~/.nvimrc
-	ln -sfT $sd/vim ~/.nvim
+	ln -sfT $sd/vimrc $HOME/.nvimrc
+	ln -sfT $sd/vim $HOME/.nvim
 }
 
 install_X() {
-	ln -sf $sd/x11/xsessionrc ~/.xsessionrc
-	ln -sf $sd/x11/xresources ~/.Xresources
+	ln -sf $sd/x11/xsessionrc $HOME/.xsessionrc
+	ln -sf $sd/x11/xresources $HOME/.Xresources
 	if [ ! -f $sd/x11/xresources.local ]; then
 		cp $sd/x11/xresources.local.default $sd/x11/xresources.local
 	fi
-	ln -sf $sd/x11/xresources.local ~/.Xresources.local
+	ln -sf $sd/x11/xresources.local $HOME/.Xresources.local
 
-	[ -d ~/.config/fontconfig ] || mkdir -p ~/.config/fontconfig
-	ln -sf $sd/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
+	[ -d $HOME/.config/fontconfig ] || mkdir -p $HOME/.config/fontconfig
+	ln -sf $sd/fontconfig/fonts.conf $HOME/.config/fontconfig/fonts.conf
 	if [ ! -f $sd/fontconfig/local.conf ]; then
 		cp $sd/fontconfig/local.conf.default $sd/fontconfig/local.conf
 	fi
-	ln -sf $sd/fontconfig/local.conf ~/.config/fontconfig/local.conf
-	rm -f ~/.fonts.conf
+	ln -sf $sd/fontconfig/local.conf $HOME/.config/fontconfig/local.conf
+	rm -f $HOME/.fonts.conf
 
-	ln -sf $sd/gtkrc-2.0 ~/.gtkrc-2.0
+	ln -sf $sd/gtkrc-2.0 $HOME/.gtkrc-2.0
 }
 
 install_xfce4_terminal() {
-	ln -sfT $sd/xfce4-terminal ~/.config/xfce4/terminal
+	ln -sfT $sd/xfce4-terminal $HOME/.config/xfce4/terminal
 }
 
 
@@ -162,11 +167,27 @@ install X
 install xfce4-terminal
 
 
-echo "Installing ~/bin files"
-[ -d ~/bin ] || mkdir ~/bin
-if [ -d /etc/apache2 ]; then
-	ln -sf $sd/bin/a2es ~/bin/a2es
+# GTK themes
+if command -v gtk-launch >/dev/null 2>&1; then
+	if [ ! -d $vd/paper-gtk-theme ]; then
+		git clone https://github.com/snwh/paper-gtk-theme $vd/paper-gtk-theme
+	fi
+	ln -sfT $vd/paper-gtk-theme/Paper $HOME/.themes/Paper
+
+	if [ ! -d $vd/zuki-themes ]; then
+		git clone https://github.com/lassekongo83/zuki-themes $vd/zuki-themes
+	fi
+	ln -sfT $vd/paper-gtk-theme/zuki-themes/Zukiwi $HOME/.themes/Zukiwi
+	ln -sfT $vd/paper-gtk-theme/zuki-themes/Zukitwo $HOME/.themes/Zukitwo
+	ln -sfT $vd/paper-gtk-theme/zuki-themes/Zukitre $HOME/.themes/Zukitre
 fi
-ln -sf $sd/bin/art ~/bin/art
-ln -sf $sd/bin/genpw ~/bin/genpw
-ln -sf $sd/bin/templ ~/bin/templ
+
+
+echo "Installing ~/bin files"
+[ -d $HOME/bin ] || mkdir $HOME/bin
+if [ -d /etc/apache2 ]; then
+	ln -sf $sd/bin/a2es $HOME/bin/a2es
+fi
+ln -sf $sd/bin/art $HOME/bin/art
+ln -sf $sd/bin/genpw $HOME/bin/genpw
+ln -sf $sd/bin/templ $HOME/bin/templ
