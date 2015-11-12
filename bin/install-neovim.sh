59d0ad791759ffe19281c8ff9dccfaff941576ac
Andreas Lutro <anlutro@gmail.com>
2015-11-12 13:18:50 +0100

    add update-alternatives commands

--- bin/install-neovim.sh
+++ bin/install-neovim.sh
@@ -8,3 +8,5 @@ sudo rm /usr/local/share/nvim/runtime/*.vim
 sudo rm /usr/local/share/nvim/runtime/syntax/*.vim
 sudo rm /usr/local/share/nvim/runtime/ftplugin/*.vim
 sudo checkinstall
+sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
+sudo update-alternatives --set vim /usr/local/bin/nvim
