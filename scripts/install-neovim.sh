16f97308183cec0578c7e2438451a4acd41e0a46
Andreas Lutro <anlutro@gmail.com>
2015-11-29 00:10:48 +0100

    remove more files

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -8,6 +8,7 @@ sudo rm /usr/local/share/nvim/runtime/*.vim
 sudo rm /usr/local/share/nvim/runtime/syntax/*.vim
 sudo rm /usr/local/share/nvim/runtime/ftplugin/*.vim
 sudo rm /usr/local/share/nvim/runtime/autoload/*.vim
+sudo rm /usr/local/share/nvim/runtime/tutor/*.tutor
 sudo checkinstall
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
 sudo update-alternatives --set vim /usr/local/bin/nvim
