ccfd2a75b109047445a35014ec8474b234169dff
Andreas Lutro <anlutro@gmail.com>
2015-12-21 04:20:10 +0100

    work on neovim install script

--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -9,6 +9,7 @@ sudo rm /usr/local/share/nvim/runtime/syntax/*.vim
 sudo rm /usr/local/share/nvim/runtime/ftplugin/*.vim
 sudo rm /usr/local/share/nvim/runtime/autoload/*.vim
 sudo rm /usr/local/share/nvim/runtime/tutor/*.tutor
-sudo checkinstall
+sudo rm /usr/local/share/nvim/runtime/indent/*.vim
+sudo checkinstall -y --pkgname neovim make install
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
 sudo update-alternatives --set vim /usr/local/bin/nvim
