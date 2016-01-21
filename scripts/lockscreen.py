dffedf03a82ac8838abd1379b6efb9c57c03ed65
Andreas Lutro <anlutro@gmail.com>
2016-01-21 18:33:41 +0100

    don't fail to lock screen when ssh-add fails

--- scripts/lockscreen.py
+++ scripts/lockscreen.py
@@ -3,7 +3,9 @@
 from os import system
 from sys import argv
 
-cmd = 'ssh-add -D && i3lock -c 000000'
+system('ssh-add -D')
+
+cmd = 'i3lock -c 000000'
 
 if '-s' in argv or '--suspend' in argv:
 	 cmd += ' && systemctl suspend'
