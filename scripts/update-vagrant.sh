cf92b43e177bbab8902422baec816859aced5930
Andreas Lutro <anlutro@gmail.com>
2016-10-20 16:35:53 +0200

    add a script to update vagrant

---
+++ scripts/update-vagrant.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+url=$(curl -sSL https://www.vagrantup.com/downloads.html | grep -o 'https://.*_x86_64\.deb')
+version=$(echo $url | sed -r 's/.*vagrant_([0-9.]+)_x86_64.*/\1/')
+filename=$(echo $url | rev | cut -d/ -f 1 | rev)
+
+if [ -z "$url" ]; then
+	echo 'Could not find _x86_64.deb!'
+	exit 1
+fi
+
+if dpkg -s vagrant | grep "Version: 1:$version" > /dev/null; then
+	echo "Latest version ($version) already installed!"
+	exit 0
+fi
+
+echo cd ~/Downloads
+echo wget $url
+echo sudo dpkg -i $filename
