68c19563eef67887488012482d9ace596a70860a
Andreas Lutro <anlutro@gmail.com>
2016-10-30 11:32:53 +0100

    support for non-debian distros

--- scripts/update-sublime-text.sh
+++ scripts/update-sublime-text.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 set -eu
 
-url=$(curl -sSL https://www.sublimetext.com/3dev | grep -o 'https://.*_amd64\.deb')
-version=$(echo $url | sed -r 's/.*build-([0-9]+)_amd64.*/\1/')
-filename=$(echo $url | rev | cut -d/ -f 1 | rev)
+rel=$(lsb_release -s -i)
+if [ $rel = 'Debian' ] || [ $rel = 'Ubuntu' ]; then
+	url=$(curl -sSL https://www.sublimetext.com/3dev | grep -oP 'https://[A-z0-9/_.-]*_amd64\.deb')
+	version=$(echo $url | sed -r 's/.*build-([0-9]+)-_amd64.*/\1/')
+else
+	url=$(curl -sSL https://www.sublimetext.com/3dev | grep -oP 'https://[A-z0-9/_.-]*_x64\.tar\.bz2')
+	version=$(echo $url | sed -r 's/.*build_([0-9]+)_x64.*/\1/')
+fi
+
+filename=$(basename $url)
 
 if [ -z "$url" ]; then
-	echo 'Could not find amd64.deb!'
+	echo 'Could not find appropriate download link!'
 	exit 1
 fi
 
-if dpkg -s sublime-text | grep "Version: $version" > /dev/null; then
+if [ $rel = 'Debian' ] || [ $rel = 'Ubuntu' ]; then
+	installed_version=$(dpkg -s sublime-text | grep "Version:" | cut -d' ' -f2)
+else
+	installed_version=$(grep -oE 'Build [0-9]+' /opt/sublime_text_3/changelog.txt | head -1 | cut -d' ' -f2)
+fi
+
+if [ $installed_version -ge $version ]; then
 	echo "Latest version ($version) already installed!"
 	exit 0
 fi
 
 cd ~/Downloads
 wget $url
-sudo dpkg -i $filename
+
+if [ $rel = 'Debian' ] || [ $rel = 'Ubuntu' ]; then
+	sudo dpkg -i $filename
+else
+	tar xf $filename
+	sudo mv -f sublime_text_3 /opt
+	sudo ln -sf /opt/sublime_text_3/sublime_text /usr/local/bin/subl
+fi
