3ff54a327fa2d4989d16725e73ac8cc764e2458f
Andreas Lutro <anlutro@gmail.com>
2017-03-31 21:05:57 +0200

    more useful message if sublime text not installed

--- scripts/sublp.sh
+++ scripts/sublp.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ! command -v subl 2>&1 > /dev/null; then
+	echo "Sublime Text (subl) not installed!"
+	exit 1
+fi
+
 dir="${1:-.}"
 sp=$(find "$dir" -maxdepth 1 -name '*.sublime-project')
 
