afa51fd4da3385b6d93753d51413e349a17c5b6c
Andreas Lutro <anlutro@gmail.com>
2017-06-23 22:19:26 +0200

    work on stream script

--- scripts/stream-twitch.sh
+++ scripts/stream-twitch.sh
@@ -1,33 +1,40 @@
 #!/bin/sh
 
-if [ ! "$1" ]; then
+if [ -n "$1" ]; then
+	STREAM_KEY="$1"
+elif [ -f ~/.twitch_stream_key ]; then
+	STREAM_KEY="$(cat ~/.twitch_stream_key)"
+fi
+
+if [ -z "$STREAM_KEY" ]; then
 	echo "Missing stream key!"
 	exit 1
 fi
-STREAM_KEY="$1"
 
 # input resolution
-INRES="1920x1080"
+CAPTURE_RESOLUTION="1920x1080"
 # output resolution
-OUTRES="960x540"
+# OUTPUT_RESOLUTION="960x540"
+OUTPUT_RESOLUTION=$CAPTURE_RESOLUTION
 # target FPS
-FPS="15"
+FPS="30"
 # i-frame interval, should be double of FPS,
-GOP="30"
-# min i-frame interval, should be equal to FPS
-GOPMIN="15"
+GOP="60"
 # max 6
 THREADS="2"
-# constant bitrate (should be between 1000k - 3000k)
-CBR="${2-1500k}"
 # one of the many FFMPEG presets
-QUALITY="ultrafast" 
+PRESET="ultrafast"
+# audio frequency, in hz
 AUDIO_RATE="44100"
+AUDIO_BITRATE="192k"
+VIDEO_BITRATE_MIN="2000k"
+VIDEO_BITRATE_MAX="3600k"
 # twitch server in frankfurt, see http://bashtech.net/twitch/ingest.php for list
-SERVER="live-arn"
+SERVER="live-ams"
 
-ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 \
-	-ar $AUDIO_RATE -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR \
-	-minrate $CBR -maxrate $CBR -pix_fmt yuv420p -s $OUTRES -preset $QUALITY \
-	-tune film -acodec libmp3lame -threads $THREADS -strict normal \
-	-bufsize $CBR "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
+ffmpeg -f x11grab -s $CAPTURE_RESOLUTION -r $FPS -i :0.0 -ar $AUDIO_RATE
+	-f alsa -i hw:1,0 -f flv -ac 2 -vcodec libx264 -g $GOP -keyint_min $FPS \
+	-b:v $VIDEO_BITRATE_MAX -minrate $VIDEO_BITRATE_MIN -maxrate $VIDEO_BITRATE_MAX \
+	-pix_fmt yuv420p -s $OUTPUT_RESOLUTION -preset $PRESET -tune film \
+	-acodec aac -b:a $AUDIO_BITRATE -threads $THREADS -strict normal \
+	-bufsize $VIDEO_BITRATE "rtmp://$SERVER.twitch.tv/app/$STREAM_KEY"
