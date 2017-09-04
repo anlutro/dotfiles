1d11cae8121df07c9212c68af592fdebc5048c07
Andreas Lutro <anlutro@gmail.com>
2017-09-04 11:35:02 +0200

    docker-compose stuff

---
+++ scripts/update-docker-compose.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+url=$(curl -s https://api.github.com/repos/docker/compose/releases | grep browser_download_url \
+	| grep Linux-x86_64 | grep -v -- '-rc' | head -1 | cut -d\" -f4)
+file=$(basename $url)
+version=$(echo $url | sed -r 's|.*/download/([0-9.-]+)/.*|\1|')
+
+if docker-compose --version | grep -F "version $version,"; then
+	echo "Latest version ($version) already installed!"
+	exit 1
+fi
+
+cd ~/Downloads
+wget $url
+chmod +x $file
+mv $file /usr/local/bin/docker-compose
