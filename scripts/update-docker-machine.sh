f78e2a9145d6a4a39779b6f16f99a0b56967ee34
Andreas Lutro <anlutro@gmail.com>
2017-10-18 09:58:36 +0200

    add update-docker-machine script

---
+++ scripts/update-docker-machine.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+url=$(curl -s https://api.github.com/repos/docker/machine/releases | grep browser_download_url \
+	| grep Linux-x86_64 | grep -v -- '-rc' | head -1 | cut -d\" -f4)
+file=$(basename $url)
+version=$(echo $url | sed -r 's|.*/download/v([0-9.-]+)/.*|\1|')
+
+if docker-machine --version 2>&1 | grep -qF "version $version,"; then
+	echo "Latest version ($version) already installed!"
+	exit 1
+fi
+
+cd ~/downloads
+wget $url
+chmod +x $file
+mv $file /usr/local/bin/docker-machine
