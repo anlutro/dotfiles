86635b0e0331e4d5110d332ae54edde1ff8094b3
Andreas Lutro <anlutro@gmail.com>
2017-11-24 11:07:00 +0100

    add script to compile git-crypt

---
+++ scripts/compile-git-crypt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+GIT_CRYPT_PATH=${1:-git-crypt}
+
+if [ ! -d $GIT_CRYPT_PATH ]; then
+	echo "[ERROR] Directory not found: $GIT_CRYPT_PATH"
+	echo "git clone https://github.com/AGWA/git-crypt $GIT_CRYPT_PATH"
+	exit 1
+fi
+
+cd $GIT_CRYPT_PATH
+if [ -d $GIT_CRYPT_PATH/.git ]; then
+	git pull
+fi
+
+make && mv git-crypt ~/bin
