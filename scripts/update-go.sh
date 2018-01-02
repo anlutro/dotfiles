037e7bd012e7c4d8d2e5321960d55026f67b4076
Andreas Lutro <anlutro@gmail.com>
2018-01-02 15:21:38 +0100

    add script to update go

---
+++ scripts/update-go.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+URL=$(
+	curl -s 'https://golang.org/dl/' | grep -F 'href=' \
+	| grep -oP 'https?://[^"]+linux-amd64\.tar\.gz' | head -1
+)
+FILENAME=$(basename $URL)
+VERSION=$(echo $FILENAME | grep -oP '\d+\.\d+\.\d+')
+PREFIX=/opt/go-${VERSION}
+
+if [ ! -d $PREFIX ]; then
+	cd ~/downloads
+	wget $URL
+	mkdir /tmp/go-${VERSION}
+	tar -C /tmp/go-${VERSION} $FILENAME
+	sudo mv /tmp/go-${VERSION} $PREFIX
+	sudo chown -R root:staff $PREFIX
+fi
+
+if ! command go version | grep -qF $VERSION 2>&1 >/dev/null; then
+	ln -sf $PREFIX/go/bin/go* /usr/local/bin
+else
+	echo "Go already at the latest version ($VERSION)"
+fi
