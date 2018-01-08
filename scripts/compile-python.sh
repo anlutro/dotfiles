ab73baf7fb9614367bbbe28fd616d7c883cd438f
Andreas Lutro <anlutro@gmail.com>
2018-01-08 17:42:41 +0100

    that is not valid shell

--- scripts/compile-python.sh
+++ scripts/compile-python.sh
@@ -13,11 +13,11 @@ if ! dpkg -l | grep -q 'libsqlite.*-dev'; then
 fi
 if ! dpkg -l | grep -q 'libreadline.*-dev'; then
 	echo "warning: libsqlite not installed!"
-	warn = yes
+	warn=yes
 fi
 if ! dpkg -l | grep -q 'libssl.*-dev'; then
 	echo "warning: libssl not installed!"
-	warn = yes
+	warn=yes
 fi
 if [ $warn = 'yes' ]; then
 	exit 1
