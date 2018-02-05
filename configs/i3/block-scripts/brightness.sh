79628a5657510993e4702defa750b07672f466de
Andreas Lutro <anlutro@gmail.com>
2018-02-05 21:14:19 +0100

    add brightness to i3blocks

---
+++ configs/i3/block-scripts/brightness.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dir=/sys/class/backlight/intel_backlight
+cur=$(cat $dir/actual_brightness)
+max=$(cat $dir/max_brightness)
+pct=$(echo "$cur / $max * 100" | bc -l)
+printf "%.1f%%\n" $pct
+printf "%.1f%%\n" $pct
+echo "#ffffff"
