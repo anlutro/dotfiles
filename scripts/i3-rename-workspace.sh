d6a2bbd3bfba84cfda2a8b9a13f5462e91f095e5
Andreas Lutro <anlutro@gmail.com>
2018-04-16 10:38:34 +0200

    don't bother showing current workspace name

--- scripts/i3-rename-workspace.sh
+++ scripts/i3-rename-workspace.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-cur_ws=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d\" -f2)
-new_name=$(echo $cur_ws | rofi -dmenu -lines 1 -p 'Rename workspace to: ')
+
+new_name=$(rofi -dmenu -input /dev/null -lines 1 -p 'Rename workspace to: ')
+
 if [ -n "$new_name" ]; then
 	echo "Renaming workspace to $new_name"
 	i3-msg rename workspace to "$new_name"
