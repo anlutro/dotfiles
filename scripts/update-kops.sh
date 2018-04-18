1e9b70d5b6e83a12f6830014472c105db1fb526e
Andreas Lutro <anlutro@gmail.com>
2018-04-18 15:39:20 +0200

    add script for updating kops

---
+++ scripts/update-kops.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+version=$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)
+
+if kops version | grep -qF "Version $version "; then
+	echo "Latest version ($version) already installed!"
+	exit 1
+fi
+
+cd ~/downloads || exit 1
+wget https://github.com/kubernetes/kops/releases/download/$version/kops-linux-amd64
+sudo cp kops-linux-amd64 /usr/local/bin/kops
+sudo chmod 755 /usr/local/bin/kops
