e829f2137950fad8d477995a2ee54687c25c64e3
Andreas Lutro <anlutro@gmail.com>
2018-09-05 20:56:30 +0200

    don't rewrite if regex doesn't match

--- configs/irssi/scripts/rewrite_fromgitter.pl
+++ configs/irssi/scripts/rewrite_fromgitter.pl
@@ -7,7 +7,9 @@ sub msg_rewrite_fromgitter_messages {
     return unless $nick eq "FromGitter";
 
     my ($target, $message) = split /:/, $data, 2;
-    my ($gitter_nick, $new_message) = $message =~ m/^\x02\<([^>]+)\>\s*[\x02\x0f]\s*(.*)$/;
+    my $match = $message =~ m/^\x02\<([^>]+)\>\s*[\x02\x0f]\s*(.*)$/;
+    return unless $match;
+    my ($gitter_nick, $new_message) = ($1, $2);
 
     Irssi::signal_continue($server, "$target:$new_message", "Gitter:$gitter_nick", $nick_and_address);
 }
