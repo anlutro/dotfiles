b003ece44469ef8a978561bb6b1d42ddbac1551d
Andreas Lutro <anlutro@gmail.com>
2018-11-15 10:13:54 +0100

    add script for updating acrophone

---
+++ scripts/update-acrophone.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+url=$(curl -s https://api.github.com/repos/bcicen/acrophone/releases | grep browser_download_url \
+	| grep linux-amd64 | head -1 | cut -d\" -f4)
+file=$(basename $url)
+version=$(echo $url | sed -r 's|.*/download/v([0-9.-]+)/.*|\1|')
+
+if acrophone --version 2>&1 | grep -qF "version $version,"; then
+	echo "Latest version ($version) already installed!"
+	exit 1
+fi
+
+if [ -w /usr/local ]; then
+	DIR=/usr/local/bin
+else
+	DIR="$HOME/bin"
+fi
+
+cd ~/downloads || exit 1
+wget $url
+cp $file $DIR/acrophone
+chmod +x $DIR/acrophone
