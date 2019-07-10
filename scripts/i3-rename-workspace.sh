f8e11f17da683c2a13d0536ac6fac8828ddf95ce
Andreas Lutro <anlutro@gmail.com>
2019-07-10 10:57:00 +0200

    prevent duplicate colons

--- scripts/i3-rename-workspace.sh
+++ scripts/i3-rename-workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-new_name=$(rofi -m -4 -dmenu -input /dev/null -lines 1 -p 'Rename workspace to: ')
+new_name=$(rofi -m -4 -dmenu -input /dev/null -lines 1 -p 'Rename workspace to')
 
 if [ -n "$new_name" ]; then
 	echo "Renaming workspace to $new_name"
