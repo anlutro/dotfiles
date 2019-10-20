4e1787028eedddf9c55cc770a65c36706c88105b
Andreas Lutro <anlutro@gmail.com>
2019-10-20 16:04:22 +0200

    disable qt auto scaling

--- configs/shell/shrc.d/0_env.sh
+++ configs/shell/shrc.d/0_env.sh
@@ -40,3 +40,6 @@ export GO111MODULE=on
 export DOCKER_BUILDKIT=1
 export DOCKER_CLI_EXPERIMENTAL=enabled
 export DOCKER_HIDE_LEGACY_COMMANDS=1
+
+# avoid weird scaling in qt5
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
