d1dad03710b9a2d7607f8e10e484583a87a50717
Andreas Lutro <anlutro@gmail.com>
2019-12-09 15:49:02 +0100

    more features!

--- scripts/i3-rename-workspace.sh
+++ scripts/i3-rename-workspace.sh
@@ -1,6 +1,19 @@
-#!/bin/sh
+#!/bin/sh -eu
 
-new_name=$(rofi -m -4 -dmenu -input /dev/null -lines 1 -p 'Rename workspace to')
+cur_name=$(i3-msg -t get_workspaces | jq -r '.[] | select (.focused==true).name')
+cur_num=$(echo $cur_name | grep -oP '^[0-9]+')
+
+if [ "${1-}" = "-d" ] || [ "${1-}" = "--dir" ]; then
+	new_name=$(basename $(pwd))
+elif [ $# -gt 0 ]; then
+	new_name="$*"
+else
+	new_name=$(rofi -m -4 -dmenu -input /dev/null -lines 1 -p 'Rename workspace to')
+fi
+
+if ! echo "$new_name" | grep -qP '^[0-9]+:'; then
+	new_name="$cur_num:$new_name"
+fi
 
 if [ -n "$new_name" ]; then
     echo "Renaming workspace to $new_name"
