3d591ad1e2d50baaf2c27bc05e8628e77026d4ff
Andreas Lutro <anlutro@gmail.com>
2020-03-05 12:09:16 +0100

    disable recommends by default!

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -20,29 +20,31 @@ deb $debian_mirror/debian/ $debian_release-backports main
 deb-src $debian_mirror/debian/ $debian_release-backports main
 EOF
 
-apt_inst='apt-get -y install --no-install-recommends'
+# don't install recommended packages by default
+echo 'apt::install-recommends "false";' > /etc/apt/apt.conf.d/99-install-recommends
 
-apt-get update && apt-get -y dist-upgrade
-$apt_inst git vim tree curl irssi zip fuse psmisc jq bc silversearcher-ag \
+apt-get update && apt-get dist-upgrade -y
+apt-get install -y git vim tree curl irssi zip fuse psmisc jq bc silversearcher-ag \
     policykit-1 sudo apt-transport-https htop gpg
 
 usermod -a -G sudo,root,adm,staff,systemd-journal andreas
 
 if confirm "Install X11, i3 and utilities?"; then
-    $apt_inst xserver-xorg-{core,input-{kbd,mouse,evdev}} x11-xserver-utils \
+    apt-get install -y xserver-xorg-{core,input-{kbd,mouse,evdev}} x11-xserver-utils \
         dbus-x11 xclip rofi dunst libnotify-bin scrot imagemagick xdg-user-dirs \
         feh rxvt-unicode-256color i3-wm i3lock i3blocks xinit xautolock \
-        alsa-utils ntfs-3g
+        alsa-utils ntfs-3g redshift dmz-cursor-theme
 
+    update-alternatives --set x-cursor-theme /usr/share/icons/DMZ-White/cursor.theme
     mv /etc/fonts/conf.d/??-user.conf /etc/fonts/conf.d/98-user.conf
     mv /etc/fonts/conf.d/??-local.conf /etc/fonts/conf.d/99-local.conf
 
     if confirm "Install laptop utilities?"; then
-        $apt_inst pm-utils xbacklight acpi network-manager redshift
+        apt-get install -y pm-utils xbacklight acpi acpi-call-dkms network-manager tlp
     fi
 
     if confirm "Install MS TrueType fonts?"; then
-        $apt_inst ttf-mscorefonts-installer
+        apt-get install -y ttf-mscorefonts-installer
     fi
 
     if confirm "Install Sublime Text?"; then
@@ -50,7 +52,7 @@ if confirm "Install X11, i3 and utilities?"; then
             > /etc/apt/trusted.gpg.d/sublimetext.gpg
         echo "deb https://download.sublimetext.com/ apt/dev/" \
             > /etc/apt/sources.list.d/sublime-text.list
-        apt-get update && $apt_inst sublime-text
+        apt-get update && apt-get install -y sublime-text
     fi
 fi
 
@@ -59,7 +61,7 @@ if confirm "Install Dropbox?"; then
         grep -oP 'href="dropbox_\d{4}.*amd64.deb"' | cut -d\" -f2 | sort | tail -1)
     wget https://linux.dropbox.com/packages/debian/$file
     dpkg -i $file
-    $apt_inst -f
+    apt-get install -y -f
     rm $file
 fi
 
@@ -68,7 +70,7 @@ if confirm "Install Docker?"; then
         > /etc/apt/trusted.gpg.d/docker.gpg
     echo "deb [arch=amd64] https://download.docker.com/linux/debian $debian_release stable" \
         > /etc/apt/sources.list.d/docker.list
-    apt-get update && $apt_inst docker-ce
+    apt-get update && apt-get install -y docker-ce
     usermod -a -G docker andreas
 fi
 
