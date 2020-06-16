75eba2b31ac2eabbb7d8a71e3b3219e0f529f00d
Andreas Lutro <anlutro@gmail.com>
2020-06-16 13:49:28 +0200

    more intelligent finding of sublime-project files

--- scripts/sublp.sh
+++ scripts/sublp.sh
@@ -9,20 +9,31 @@ fi
 dir=$(readlink -f "${1-$PWD}")
 cd $dir || exit 1
 
-# find the sublime-project file
-sp=$(find "$dir" -maxdepth 1 -name '*.sublime-project')
+find_sublime_project_file() {
+    find "$1" -maxdepth 1 -name '*.sublime-project'
+}
+
+sp="$(find_sublime_project_file "$dir")"
+
+# if not found, try looking in parent directories, but not outside of $HOME
+while [ -z "$sp" ] && echo "$dir" | grep -q "^$HOME"; do
+    dir="$(dirname "$dir")"
+    sp="$(find_sublime_project_file "$dir")"
+done
+
 if [ -z "$sp" ]; then
     echo "No sublime-project found, running init-project ..."
     init-project --noninteractive --allow-no-type
-    sp=$(find "$dir" -maxdepth 1 -name '*.sublime-project')
+    sp="$(find_sublime_project_file "$dir")"
 fi
 if [ -z "$sp" ]; then
-    sp="$dir"
+    echo "No sublime-project file found, just opening as directory"
+    sp="${1-$PWD}"
 fi
 
 # rename i3 workspace
 if command -v i3-rename-workspace >/dev/null 2>&1; then
-	i3-rename-workspace --dir $dir
+    i3-rename-workspace --dir "$dir"
 fi
 
 # open sublime text
