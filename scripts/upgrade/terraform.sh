47cdba4d819e58ea5ab40e0797bd19a463085e30
Andreas Lutro <anlutro@gmail.com>
2020-08-21 09:52:35 +0200

    allow specifying terraform version

--- scripts/upgrade/terraform.sh
+++ scripts/upgrade/terraform.sh
@@ -3,8 +3,12 @@ set -eu
 # shellcheck source=_lib.sh
 . "$(dirname "$(readlink -f "$0")")/_lib.sh"
 
-url=$(curl -sSL https://www.terraform.io/downloads.html | grep -oP 'https://[A-z0-9/_.-]*_linux_amd64\.zip')
-version=$(echo $url | sed -r 's/.*terraform_([0-9.]+)_linux_amd64.*/\1/')
+version=$(
+	curl -sSL https://releases.hashicorp.com/terraform/ \
+	| grep -oP 'terraform_[\d.]+' | sed 's/^terraform_//' \
+	| sort -V | uniq | grep "${1-.*}" | tail -1
+)
+url="https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
 
 if terraform --version | grep -qxF "Terraform v$version"; then
 	latest_already_installed
