3b59836abf68d6b37429fc651a85a575a443f727
Andreas Lutro <anlutro@gmail.com>
2020-09-29 16:35:09 +0200

    avoid importing site for even better performance

--- scripts/i3-get.py
+++ scripts/i3-get.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /home/andreas/code/dotfiles/.venv/bin/python
+#!/usr/bin/env -S /home/andreas/code/dotfiles/.venv/bin/python -S
 """
 Get the window ID of the next or previous window in the current workspace.
 
