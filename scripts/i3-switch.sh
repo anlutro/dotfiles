862d86b162ef4456e610e28f2c4ece892e6e92f3
Andreas Lutro <anlutro@gmail.com>
2021-01-23 17:28:38 +0100

    suppress output of i3-msg

--- scripts/i3-switch.sh
+++ scripts/i3-switch.sh
@@ -2,4 +2,4 @@
 
 dir=$(dirname $(readlink -f $0))
 id=$(i3-msg -t get_tree | $dir/i3-get.py ${1-next})
-[ $? = 0 ] && [ "$id" ] && i3-msg [id="$id"] focus
+[ $? = 0 ] && [ "$id" ] && i3-msg [id="$id"] focus >/dev/null
