e2899ba01297f25e8873499b9d66dd56a968ff53
Andreas Lutro <anlutro@gmail.com>
2021-03-10 12:06:36 +0100

    add alias ag=rg

--- configs/shell/shrc.d/aliases.sh
+++ configs/shell/shrc.d/aliases.sh
@@ -9,8 +9,11 @@ alias zgrep='zgrep --color=auto'
 # I always have to google this one
 alias grep-non-unicode='grep -P -n "[^\x00-\x7F]"'
 
-# ag doesn't have config files
-if which ag >/dev/null 2>&1; then
+if which rg >/dev/null 2>&1; then
+	# bad for muscle memory maybe but whatever
+	alias ag=rg
+elif which ag >/dev/null 2>&1; then
+	# ag doesn't have config files
     alias ag="ag --color-path='0;37' --color-line-number='0;33' --color-match='1;34'"
 fi
 
