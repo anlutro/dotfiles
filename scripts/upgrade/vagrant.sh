2cdd1f847efc2fc5c9e478eaccaacfa2ee597578
Andreas Lutro <anlutro@gmail.com>
2021-05-18 08:54:37 +0200

    download page lists many .debs

--- scripts/upgrade/vagrant.sh
+++ scripts/upgrade/vagrant.sh
@@ -3,7 +3,7 @@ set -eu
 # shellcheck source=_lib.sh
 . "$(dirname "$(readlink -f "$0")")/_lib.sh"
 
-url=$(curl -sSL https://www.vagrantup.com/downloads.html | grep -oP 'https://[/A-z0-9._-]+_x86_64\.deb')
+url=$(curl -sSL https://www.vagrantup.com/downloads.html | grep -oP 'https://[/A-z0-9._-]+_x86_64\.deb' | sort -V | tail -1)
 version=$(echo $url | sed -r 's/.*vagrant_([0-9.]+)_x86_64.*/\1/')
 
 if [ -z "$url" ]; then
