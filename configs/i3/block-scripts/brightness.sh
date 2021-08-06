2ea8003e929c2ac383bc61d4a3052e5768a8bb28
Andreas Lutro <anlutro@gmail.com>
2021-08-06 14:38:44 +0200

    avoid printing 0.0%

--- configs/i3/block-scripts/brightness.sh
+++ configs/i3/block-scripts/brightness.sh
@@ -5,6 +5,9 @@ dir=/sys/class/backlight/intel_backlight
 cur=$(cat $dir/actual_brightness)
 max=$(cat $dir/max_brightness)
 pct=$(echo "$cur / $max * 100" | bc -l)
+if echo "$pct" | grep -qP '^0?.0'; then
+	pct=0.1
+fi
 printf "Brightness %.1f%%\n" $pct
 printf "Brightness %.1f%%\n" $pct
 echo "#ffffff"
