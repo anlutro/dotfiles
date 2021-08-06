ed7608510779c01a3912db8de898330a1c1272c2
Andreas Lutro <anlutro@gmail.com>
2021-08-06 14:40:55 +0200

    fix regex?

--- configs/i3/block-scripts/brightness.sh
+++ configs/i3/block-scripts/brightness.sh
@@ -5,7 +5,7 @@ dir=/sys/class/backlight/intel_backlight
 cur=$(cat $dir/actual_brightness)
 max=$(cat $dir/max_brightness)
 pct=$(echo "$cur / $max * 100" | bc -l)
-if echo "$pct" | grep -qP '^0?.0'; then
+if echo "$pct" | grep -qP '^0?.0\d+$'; then
 	pct=0.1
 fi
 printf "Brightness %.1f%%\n" $pct
