ad4e73f12876842a272e5d127ccc5cd2e134c07f
Andreas Lutro <anlutro@gmail.com>
2021-09-14 12:01:50 +0300

    don't set ansible env vars

--- configs/shell/shrc.d/00-env.sh
+++ configs/shell/shrc.d/00-env.sh
@@ -47,8 +47,5 @@ export DOCKER_HIDE_LEGACY_COMMANDS=1
 # avoid weird scaling in qt5
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 
-# ansible
-export ANSIBLE_VAULT_PASSWORD_FILE=./.ansible-vault-password
-
 # rg / ripgrep
 export RIPGREP_CONFIG_PATH=~/.config/ripgreprc
