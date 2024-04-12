d52af1d4481812b2c3ce325566f66899b8f39e26
Andreas Lutro <anlutro@gmail.com>
2024-04-12 13:50:42 +0200

    make brightness work with amdgpu

--- configs/i3/block-scripts/brightness.sh
+++ configs/i3/block-scripts/brightness.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-dir=/sys/class/backlight/intel_backlight
-[ -e "$dir" ] || exit 1
-cur=$(cat $dir/actual_brightness)
-max=$(cat $dir/max_brightness)
-pct=$(echo "$cur / $max * 100" | bc -l)
+for try_dir in intel_backlight amdgpu_bl0; do
+    if [ -e "/sys/class/backlight/$try_dir" ]; then
+        dir="/sys/class/backlight/$try_dir"
+        break
+    fi
+done
+if [ -z "$dir" ]; then exit 1; fi
+
+cur="$(cat "$dir/actual_brightness")"
+max="$(cat "$dir/max_brightness")"
+pct="$(echo "$cur / $max * 100" | bc -l)"
 if echo "$pct" | grep -qP '^0?.0\d+$'; then
-	pct=0.1
+    pct=0.1
 fi
 printf "Brightness %.1f%%\n" $pct
 printf "Brightness %.1f%%\n" $pct
