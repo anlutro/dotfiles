10b67cc7ffabae15a9d9b69f942ba14372b125c8
Andreas Lutro <anlutro@gmail.com>
2024-05-15 22:00:12 +0200

    diary tweak

--- scripts/diary.sh
+++ scripts/diary.sh
@@ -15,12 +15,13 @@ tmpfile=$DIARY_FILE.draft
 vim -c 'setlocal tw=79' $tmpfile
 
 if [ -e $tmpfile ] && [ -s $tmpfile ]; then
+    dt=$(date -R)
     if [ -s "$DIARY_FILE" ]; then
         printf "\n\n" >> "$DIARY_FILE"
     fi
-    echo "=====  $(date -R)  =====" >> "$DIARY_FILE"
+    echo "=====  $dt  =====" >> "$DIARY_FILE"
     cat $tmpfile >> "$DIARY_FILE"
-    echo "Appended entry to $DIARY_FILE"
+    echo "Appended entry to $DIARY_FILE ($dt)"
 else
     echo "Entry file empty, not doing anything"
 fi
