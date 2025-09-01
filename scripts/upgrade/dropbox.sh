aefe487db2df71c0d6b09bb6e1b03f989b447a68
Andreas Lutro <anlutro@gmail.com>
2025-09-01 10:58:29 +0200

    add timeout

--- scripts/upgrade/dropbox.sh
+++ scripts/upgrade/dropbox.sh
@@ -5,7 +5,7 @@ rm -rf $HOME/.dropbox-dist-old-*
 rm -rf /tmp/.dropbox-dist-*
 
 echo "Checking for Dropbox updates ..." >&2
-url=$(curl -ILs --http1.1 -o /dev/null -w '%{url_effective}' 'https://www.dropbox.com/download?plat=lnx.x86_64')
+url=$(curl -ILs --http1.1 --connect-timeout 10 -o /dev/null -w '%{url_effective}' 'https://www.dropbox.com/download?plat=lnx.x86_64')
 latest_version=$(echo "$url" | sed -r 's/.*-([0-9\.]+)\.tar\.gz/\1/')
 
 if [ -e $HOME/.dropbox-dist ]; then
